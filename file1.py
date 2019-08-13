d1567f502f6f85b8641a524dfb1aca752ce7432f
dupadhyaya <dup1966@gmail.com>
2019-08-13 13:58:13 +0530

    a

---
+++ file1.py
@@ -0,0 +1,24 @@
+#Topic ----
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#from pydataset import data
+#mtcars = data('mtcars')
+#data=mtcars
+#read data
+path='e:/adata/xxxx.csv'
+data = pd.read_csv(path)
+data.head()
+data.columns
+data.dtypes
+data.shape
+#%%%
+
+
+
+#%%%
+
+
+
+#%%%
+
