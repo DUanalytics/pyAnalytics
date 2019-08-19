b5d8aa7ce96bc2f42836e0f10bdd1d47a1a30c4d
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-08-19 07:54:00 +0530

    a

---
+++ 9B-CaseStudy/case5A_student.py
@@ -0,0 +1,12 @@
+#Topic:Pandas merge, missing values, summarise, plot
+#-----------------------------
+#libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#datasets
+#main master details: s1master
+#secondary list of studetns: s2master
+#first list of marks :m1master
+#second list of marks : m2master =
