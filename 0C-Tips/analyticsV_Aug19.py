7e34c67042e290cdace671ddb1d6a76289acda30
dupadhyaya <dup1966@gmail.com>
2019-08-20 18:11:05 +0530

    a

---
+++ 0C-Tips/analyticsV_Aug19.py
@@ -0,0 +1,25 @@
+#Topic ----TIps
+#https://www.analyticsvidhya.com/blog/2019/08/10-powerful-python-tricks-data-science/?utm_source=feedburner&utm_medium=email&utm_campaign=Feed%3A+AnalyticsVidhya+%28Analytics+Vidhya%29
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#from pydataset import data
+#mtcars = data('mtcars')
+#data=mtcars
+#read data
+path='e:/adata/xxxx.csv'
+data = pd.read_csv(path)
+data.head()
+data.columns
+data.dtypes
+data.shape
+#%%%
+
+
+
+#%%%
+
+
+
+#%%%
+
