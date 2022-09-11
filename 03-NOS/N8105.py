ebdf4cf4ed7a6f7dadad3c4d60b92082e0e8ec70
dupadhyaya <dup1966@gmail.com>
2022-09-11 20:10:01 +0530

    NOS

---
+++ 03-NOS/N8105.py
@@ -0,0 +1,21 @@
+#python : DUP - Topic :N8105 Machine Learning Algorithsm
+
+#standard libaries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#!pip install pydataset
+from pydataset import data
+import seaborn as sns
+
+##Define Hypothesis
+#PC1: identify the objective of the analysis
+#PC2 : evaluate the dataset to determine a suitable approach
+#PC3: identify suitable libraries, packages, frameworks, applications to address the objective
+##Apply and Optimise Model
+#PC4: select suitable algorithmic models from available statistical analysis softwares, packages, libraries or tools
+#PC5: apply the model for various use cases and scenarios such as vision, text recognition, image recognition, natural language processing etc
+#PC6: optimize selected algorithmic models to resolve any shortcomings or defects
+#PC7: iterate the model in consultation with relevant stakeholders till the desired performance or quality of output is achieved
+#PC8: validate the models implemented using appropriate tools and processes
+#PC9: create documentation on applied algorithmic models for future references and versioning
