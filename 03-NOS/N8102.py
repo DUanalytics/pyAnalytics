7e38c443d6626e0cfa9d191ac5877f4289a03adc
dupadhyaya <dup1966@gmail.com>
2022-09-11 10:40:30 +0530

    NOS

---
+++ 03-NOS/N8102.py
@@ -0,0 +1,8 @@
+#python : DUP - Topic :N8102 Missing data
+
+#standard libaries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pydataset import data
+import seaborn as sns
