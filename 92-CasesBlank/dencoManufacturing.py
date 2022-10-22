2df0bc65de536db6aa261b2a8cdc167e6aebd6ff
dupadhyaya <dup1966@gmail.com>
2022-10-22 10:02:31 +0530

    a

--- 92-CasesBlank/dencoManufacturing.py
+++ 92-CasesBlank/dencoManufacturing.py
@@ -21,3 +21,33 @@ pd.options.display.float_format = '{:.2f}'.format
 url='https://raw.githubusercontent.com/DUanalytics/datasets/master/csv/denco.csv'
 #df = pd.read_csv('data/denco.csv')
 df = pd.read_csv(url)
+
+
+#%%%% : Summary1
+#Who are the most loyal Customers - Improve repeated sales, Target customers with low sales Volumes
+#count by customers, sort, pick top 5 
+#Series
+
+#%%% - Summary2
+#Which customers contribute the most to their revenue - How do I retain these customers & target incentives
+#find total revenue of each customer, sort in descending.
+
+#%%% Summary3
+#What part numbers bring in to significant portion of revenue - Maximise revenue from high value parts
+#grouby partnum, find value of revenue, sell these more
+
+
+
+#%%% Summary4
+#What parts have the highest profit margin - What parts are driving profits & what parts need to build further
+#check for margin value, their individual margin and total sales margin like revenue
+
+
+#%%% Extras
+#Most sold items
+
+#%%% Extras - Plan more
+
+
+
+#end here
